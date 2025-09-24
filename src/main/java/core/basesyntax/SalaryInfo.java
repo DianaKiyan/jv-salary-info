898ad9504dde05cd62eb539475b7d560ddf83a01
Diana <dianakiyan2k20@gmail.com>
2025-09-24 14:33:02 +0300

    added data formating

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,7 +1,32 @@
 package core.basesyntax;
 
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.text.ParseException;
+import java.util.Date;
+
 public class SalaryInfo {
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        return null;
+        SimpleDateFormat inputFormat = new SimpleDateFormat("dd.MM.yyyy");
+
+        try {
+            Date dateFormatFrom = inputFormat.parse(dateFrom);
+            Date dateFormatTo = inputFormat.parse(dateTo);
+        } catch (ParseException e) {
+            System.err.println("Помилка розбору дати: " + e.getMessage());
+        }
+
+        for (String dataSep : data) {
+            String[] result = dataSep.split(" ");
+
+            String dateStr = result[0];
+            SimpleDateFormat dateStrForm = new SimpleDateFormat("dd.MM.yyyy");
+
+            String name = result[1];
+            int hours = Integer.parseInt(result[2]);
+            int rate = Integer.parseInt(result[3]);
+        }
     }
 }
